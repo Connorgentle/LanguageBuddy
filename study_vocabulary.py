b1096f24585081bb5038467b887600780d6cb825
Connorgentle <gentleconnor123@gmail.com>
2025-02-06 18:20:23 -0500

    Added instructions for flashcards

--- study_vocabulary.py
+++ study_vocabulary.py
@@ -26,8 +26,10 @@ def display_vocabulary():
     if st.session_state.vocabulary_df.empty:
         st.write("Your vocabulary list is empty. Start learning new words!")
     else:
-        with st.expander(f"View your {native_language}-{target_language} Vocabulary"):
+        with st.expander(f"View your {native_language}-{target_language} Vocabulary (you can download this as a csv by hovering over the table then clicking the download icon in the top right)"):
             st.dataframe(st.session_state.vocabulary_df, use_container_width=True)
+        with st.expander("Flashcard instructions"):
+            st.text("Select the fluency level of words you would like to study. If you are just starting on LanguageBuddy, this will be '1 - new'. Click Begin Flashcard Session and 10 flashcards will be generated below from randomly selected words in your vocabulary of that fluency type. You can practice your pronunciation by listening to the audio clip. When you want to see the answer, hit Show translation. To update the fluency level for the word, select one of the radio buttons. After the last flashcard, you will see an Update Fluency button. Press it to update your personal vocabulary list in the cloud. (note this will not update the dataset shown above, only your data in the cloud.)")
 
 @st.cache_data
 def get_translation(text):
